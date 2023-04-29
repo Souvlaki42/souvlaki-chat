e01771605556cf5cdeb30a09d1186dad7b689e0b
Souvlaki42 <souvlaki420@gmail.com>
2023-04-29 22:00:57 +0300

    Posting loading states + toast

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -40,7 +40,7 @@ export const postsRouter = createTRPCRouter({
       })
     ).map(filterUserForClient);
 
-    console.log(users);
+    // console.log(users);
 
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
@@ -61,7 +61,7 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().emoji().min(1).max(255),
+        content: z.string().emoji("Only emojis are allowed").min(1).max(255),
       })
     )
     .mutation(async ({ ctx, input }) => {
