67645324805ba4eebe97be87afc6d6b233dc2525
Souvlaki42 <souvlaki420@gmail.com>
2023-04-30 20:29:24 +0300

    Reverted back to the previous commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { PageLayout } from "~/components/layout";
 import { PostView } from "~/components/postview";
-import Link from "next/link";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -72,6 +71,8 @@ const CreatePostWizard = () => {
   );
 };
 
+
+
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
@@ -80,7 +81,7 @@ const Feed = () => {
   if (!data) return <div>Something went wrong</div>;
 
   return (
-    <div className="flex grow flex-col overflow-y-scroll">
+    <div className="flex flex-col">
       {data.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
@@ -107,38 +108,6 @@ const Home: NextPage = () => {
           {isSignedIn && <CreatePostWizard />}
         </div>
         <Feed />
-        <div className="flex items-center justify-between p-4 text-xl">
-          <Link href="https://github.com/souvlaki42/souvlaki-chat">
-            <div className="flex items-center justify-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="white"
-              >
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-              </svg>
-              <div>Github</div>
-            </div>
-          </Link>
-          <div>
-            <Link href="https://twitter.com/souvlaki42">
-              <div className="flex items-center justify-center gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="#1DA1F2"
-                >
-                  <path d="M23.25 4.5a9.375 9.375 0 01-2.7.739A4.687 4.687 0 0022.406 3.3a9.315 9.315 0 01-2.955 1.129 4.656 4.656 0 00-8 3c0 .367.041.725.1 1.068a13.238 13.238 0 01-9.605-4.868 4.663 4.663 0 00-.636 2.34 4.665 4.665 0 002.078 3.875 4.696 4.696 0 01-2.116-.586v.057c0 2.411 1.7 4.424 3.962 4.878-.414.112-.85.17-1.308.17-.32 0-.632-.031-.941-.089a4.685 4.685 0 004.352 3.25 9.394 9.394 0 01-5.776 1.993c-.376 0-.744-.021-1.111-.063A13.193 13.193 0 010 19.032a13.275 13.275 0 007.164 2.1c8.625 0 13.334-7.334 13.334-13.702 0-.208-.005-.416-.014-.622a9.74 9.74 0 002.394-2.488z" />
-                </svg>
-                <div>Twitter</div>
-              </div>
-            </Link>
-          </div>
-        </div>
       </PageLayout>
     </>
   );
